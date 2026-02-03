2c7e2d68460ca8c070113a9192b5e577893da159
hp <nabeeha529@gmail.com>
2026-02-03 22:14:04 +0500

    Simplify vite config - remove dev middleware

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    middlewareMode: false,
-    middleware: [
-      (req, res, next) => {
-        if (!req.url.match(/\.[a-z0-9]+$/i) && !req.url.startsWith('/api')) {
-          req.url = '/index.html'
-        }
-        next()
-      }
-    ]
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    sourcemap: false
   }
 })
